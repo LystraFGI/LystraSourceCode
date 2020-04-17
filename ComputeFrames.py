636e74a232ec047058da2be6c8a8f62eaaf8946e
LystraFGI <38429287+LystraFGI@users.noreply.github.com>
2020-04-17 09:15:40 +0300

    Added annotation

--- ComputeFrames.py
+++ ComputeFrames.py
@@ -1,22 +1,34 @@
+'''
+Computes ancient elevation models based on present day DEM and total occured land uplift derived from
+current land uplift rates and shoreline isobases by scaling the data with formula of exponential relaxation.
+'''
 
 from osgeo import gdal
 import numpy as np
+from tiftools import array2raster
 
-data = gdal.Open(fp_DEM_0)
+#filepaths to DEM_0 and U_0
+fp_DEM_0 = ''
+fp_U_0 = ''
 
-dem = data.ReadAsArray()
+#present-day elevation and bathymetry
+dataDEM = gdal.Open(fp_DEM_0)
+dem = dataDEM.ReadAsArray()
 
-data2 = gdal.Open(fp_U_0)
+#raster of occured land uplift since the Ancylus Lake stage
+dataU = gdal.Open(fp_U_0)
+anc = dataU.ReadAsArray()
 
-anc = data2.ReadAsArray()
-
+#constants obtained from exponential fit
 a = 9.4000712009755716E+03
 b = 3.2624206787165413E+03
 c = 7.4006662440556579E+02
 
+#scale denominator
 scale10458bp = a * (np.exp(10458/b) -1) + c
 
-for i in range(-10000, 0, 5):
+#compute frames with given timeframe and timestep
+for i in range('STARTYEAR', 'ENDYEAR', 5):
     scale_factor =  (a * (np.exp(-i/b) -1) + c) / scale10458bp
-    uusi = dem - anc * scale_factor
-    array2raster(outfilepath + str(i) + ".tif", data, uusi, "Float32")
+    newDEM = dem - anc * scale_factor
+    array2raster('outfilepath' + str(i) + ".tif", dataDEM, newDEM, "Float32")
